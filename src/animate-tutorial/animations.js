9a4cf866aae65abfcaa03cb7bbd29156569312de
AndrewSimons3 <simdrew07@gmail.com>
2019-10-01 20:31:28 -0500

    final animation proj

--- src/animate-tutorial/animations.js
+++ src/animate-tutorial/animations.js
@@ -1,9 +1,7 @@
 function openNav() {
   // this is where you do stuff
-  // show me nav-items mobile
+  // show me nav-item--mobile
   let navMobile = document.querySelector('.nav-items--mobile')
   navMobile.classList.toggle('showNav')
-  //console.log('navMobile:', navMobile)
-
-
+  // console.log('navMobile:', navMobile.classList)
 }
